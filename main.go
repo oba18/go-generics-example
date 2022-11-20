f841fc20742c18c3da7a83f7b6b00eaf138f0e7f
oba18 <m.oba.0426@gmail.com>
2022-11-20 18:25:01 +0900

    test branch 25 commit

--- main.go
+++ main.go
@@ -6,24 +6,6 @@ import (
 
 func main() {
 	fmt.Println("start")
-	aaa()
-	bbb()
-	ccc()
 	fmt.Println("Hello World")
 	fmt.Println("Complete!!!!!")
-}
-
-func aaa() {
-	aaa := "aaa"
-	fmt.Println(aaa)
-}
-
-func bbb() {
-	bbb := "bbb"
-	fmt.Println(bbb)
-}
-
-func ccc() {
-	ccc := "ccc"
-	fmt.Println(ccc)
 }
