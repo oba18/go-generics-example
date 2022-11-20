5369ed3c25f14dca8f99481762fd20500e6fc737
oba18 <m.oba.0426@gmail.com>
2022-11-20 17:36:34 +0900

    test branch 11 commit

--- main.go
+++ main.go
@@ -6,27 +6,5 @@ import (
 
 func main() {
 	fmt.Println("Hello World")
-	testBranch()
-	testBranch2()
-	testBranch3()
-	testBranch4()
 	fmt.Println("Complete!!!!!")
-}
-
-func testBranch() {
-	fmt.Println("Good Morning")
-}
-
-func testBranch2() string {
-	s := "Good Evening"
-	return s
-}
-
-func testBranch3() []int {
-	is := []int{1, 2, 3}
-	return is
-}
-
-func testBranch4() any {
-	return "aaaaaaaaaaaaa"
 }
